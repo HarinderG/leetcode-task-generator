bb67f0e09fe146280af9f1d8db482e571ed8101d
Harinder Gakhal <harinder.gakhal@gmail.com>
2021-10-30 14:22:42 -0700

    Add main func

--- FetchRandProblem.py
+++ FetchRandProblem.py
@@ -1,25 +1,35 @@
 import requests as req
 import random
 
-# This header value is used in the request to fetch personal progress.
-from const import USER_COOKIE
-# TODO Add instructions on how to obtain this
 
-res = req.get('https://leetcode.com/api/problems/algorithms/', headers={'cookie': USER_COOKIE}).json()
-# TODO: Add option to switch between topics: [algorithms, database, concurrency, etc..]
+def getRandLeetCode():
+	# This header value is used in the request to fetch personal progress.
+	from const import USER_COOKIE
+	# TODO Add instructions on how to obtain this
 
-# For debugging
-print(res['user_name'])
-# TODO Display current progress
+	res = req.get('https://leetcode.com/api/problems/algorithms/', headers={'cookie': USER_COOKIE}).json()
+	# TODO: Add option to switch between topics: [algorithms, database, concurrency, etc..]
 
-problem_list = res['stat_status_pairs']
+	# For debugging
+	print(res['user_name'])
+	# TODO Display current progress
 
-# Pick an unsolved problem
-# TODO Add option to filter out paid only probs
-# TODO Display difficulty level
-problem = random.choice(problem_list)
-while problem['status'] == 'ac':
-    print(f'You have alread completed: {problem["stat"]["question__title_slug"]}')
-    problem = random.choice(problem_list)
+	problem_list = res['stat_status_pairs']
 
-print(f'Link: https://leetcode.com/problems/{problem["stat"]["question__title_slug"]}')
+	# Pick an unsolved problem
+	# TODO Add option to filter out paid only probs
+	# TODO Display difficulty level
+	problem = random.choice(problem_list)
+	while problem['status'] == 'ac':
+		print(f'You have alread completed: {problem["stat"]["question__title_slug"]}')
+		problem = random.choice(problem_list)
+
+	print(f'Link: https://leetcode.com/problems/{problem["stat"]["question__title_slug"]}')
+
+
+def main():
+	getRandLeetCode()
+
+
+if __name__ == "__main__":
+	main()
